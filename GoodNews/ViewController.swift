587e1707497585088cc6f8324c8d3ec2c7b5aa24
ezequiel3434 <ezequiel3434@gmail.com>
2020-12-05 11:52:04 -0300

    Initial Commit

---
+++ GoodNews/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GoodNews
+//
+//  Created by Ezequiel Parada Beltran on 05/12/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
