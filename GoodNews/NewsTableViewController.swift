dbdf18c1137b6e518a389ee656f1c2cd7156753e
ezequiel3434 <ezequiel3434@gmail.com>
2020-12-05 20:14:51 -0300

    fetching news from newsApi

--- GoodNews/NewsTableViewController.swift
+++ GoodNews/NewsTableViewController.swift
@@ -6,6 +6,8 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
 class NewsTableViewController: UITableViewController {
     override func viewDidLoad() {
@@ -13,6 +15,26 @@ class NewsTableViewController: UITableViewController {
         self.navigationController?.navigationBar.prefersLargeTitles = true
         self.navigationController?.setStatusBar(backgroundColor: UIColor(displayP3Red: 47/255, green: 54/255, blue: 64/255, alpha: 1.0))
     }
+    
+    private func populateNews(){
+        let url = URL(string: "https://newsapi.org/v2/top-headlines?country=us&apiKey=0f592d1c093f4f56885e639c2b770da5")
+        
+        Observable.just(url)
+            .flatMap { (url) -> Observable<Data> in
+                let request = URLRequest(url: url!)
+                return URLSession.shared.rx.data(request: request)
+            }.map { (data) -> [Article]? in
+                return try? JSONDecoder().decode(ArticleList.self, from: data).articles
+            }.subscribe(onNext:{ [weak self] articles in
+                guard let self = self else {return}
+                if let articles = articles {
+                    DispatchQueue.main.async {
+                        self.tableView.reloadData()
+                    }
+                }
+            })
+    }
+    
 }
 
 
